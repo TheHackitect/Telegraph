b4dd622cb44f885abe9418396e5712f64545f5dd
TheHackitect <marvellouskolawole.mk@gmail.com>
2022-04-02 12:09:52 +0100

    .

--- Telegraph.py
+++ Telegraph.py
@@ -2,11 +2,32 @@ import requests
 from telegraph import Telegraph
 telegraph = Telegraph()
 telegraph.create_account(short_name='1337')
-with open('image.jpeg', 'rb') as f:
-    path = requests.post(
-                    'https://telegra.ph/upload', files={'file':('file', f, 'image/jpeg')}).json()[0]['src']
-response = telegraph.create_page('Hey',html_content="<img src='{}'/><p>Hello, world!</p> ".format(path),)
+image = open('img.jpg', 'rb')
+video = open('vid.mp4', 'rb')
+src = requests.post('https://telegra.ph/upload', files={'file1':('image', image, 'image/jpeg')}).json() # A list containnig dict(s)
+img_src = src[0]["src"]
+response = telegraph.create_page('FutariansBot',html_content=f"Welcome!<br><img src={img_src}>")
+print((response))
 
+# 
+"""
 
-
+response = telegraph.create_page('Thehackitect',html_content="".format(path))
+url = 'https://telegra.ph/upload'
+payload = files
+headers = {}
+response = requests.request("POST", url, headers=headers, data=payload)
+# reponse
+# {'path': 'Hey-03-23-28', 'url': 'https://telegra.ph/Hey-03-23-28', 'title': 'Hey', 'description': '', 'views': 0, 'can_edit': True}
+print(response["url"])
 print('http://telegra.ph/{}'.format(response['path']))
+"""
+
+
+memo = """line 1
+line 2
+
+line 3"""
+
+out = '<br>'.join(memo.splitlines())
+print(out)
